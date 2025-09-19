8bf30675b1cc2ca677a2421b6ffd90da00c4db51
Everest Paudel <paudeleverest09@gmail.com>
2025-09-19 22:54:51 +0545

    Backend almost done

--- backend/src/routes/trasactionRoute.js
+++ backend/src/routes/trasactionRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { sql } from "../config/db.js";
+
 import { createTransaction, deleteTransactionm, getTransactionByUserId, getUserAccountSummary } from '../controllers/transaction-controller.js';
 const router = express.Router()
 
