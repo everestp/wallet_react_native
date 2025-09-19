876d6c93e504d2155471b31cb502051e1a31671a
Everest Paudel <paudeleverest09@gmail.com>
2025-09-19 23:00:49 +0545

    Fix :Backend refactor

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,3 +3,23 @@ import {neon} from '@neondatabase/serverless'
 import 'dotenv/config'
 //create  a SQL connection  using our DB url
 export const sql = neon(process.env.DATABASE_URL)
+
+ export async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10 ,2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+            
+
+            
+            )`;
+    console.log("Database intialized sucessfully");
+  } catch (error) {
+    console.log("Error intilaizing DB ", error);
+    process.exit(1);
+  }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { sql } from "./config/db.js";
+import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import trasactionRoute from "./routes/trasactionRoute.js"
 dotenv.config();
@@ -9,25 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(rateLimiter)
 const PORT = process.env.PORT || 5001;
-async function initDB() {
-  try {
-    await sql`CREATE TABLE IF NOT EXISTS transactions(
-            id SERIAL PRIMARY KEY,
-            user_id VARCHAR(255) NOT NULL,
-            title VARCHAR(255) NOT NULL,
-            amount DECIMAL(10 ,2) NOT NULL,
-            category VARCHAR(255) NOT NULL,
-            created_at DATE NOT NULL DEFAULT CURRENT_DATE
-            
-
-            
-            )`;
-    console.log("Database intialized sucessfully");
-  } catch (error) {
-    console.log("Error intilaizing DB ", error);
-    process.exit(1);
-  }
-}
 
 
 app.use("/api/transactions", trasactionRoute)
